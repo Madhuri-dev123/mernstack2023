64fc7d849adac7828d03b68e7ccf1008156638a3
madhuri-dev123 <gunnapanani.madhuri@gmail.com>
2025-02-15 16:02:50 +0530

    datatypes

---
+++ javascript/datatypes.js
@@ -0,0 +1,31 @@
+// data types
+// 1.promitive 
+//a.numbers ; both integers and foltes
+
+let a = 10;
+let b = 4.15 ;
+console.log(a)
+console.log(b)
+console.log(typeof(a))
+
+// b. booleans ; logical values true or false 
+
+//c. undefined ; in code declared but not assiged any value in program,
+  let x;
+  console.log(x)
+
+  // d.null; 
+
+//2. non-primitive
+    //a. objects ; collection of key and values pair
+
+     let obj = 
+        {name:"madhduri",age:24,branch:"cse"}
+     
+     console.log(obj.age);
+    //b. arrays ; to store the data index type formating.
+    // 1. index values stating with "0"
+
+      let array = [2,33,42,12,16,10];
+      console.log(array)
+            
