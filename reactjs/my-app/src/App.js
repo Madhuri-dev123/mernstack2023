90c8d0d737826677470a69ecf236aabded50119e
madhuri-dev123 <gunnapanani.madhuri@gmail.com>
2025-02-25 12:22:02 +0530

    fvnv

--- reactjs/my-app/src/App.js
+++ reactjs/my-app/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { Second, Third, } from './components/secondcomponent';
 import FistComponent from './components/firstcomponent';
-
+no
 function App() {
   return (
   <>
